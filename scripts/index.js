f8623957f727ff2fe6a415a40cdc963626ef6b5c
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-02-06 15:54:37 +0000

    Sdfadk

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,27 @@
 ﻿(function() {
-
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-	var pictureSource;
-	var destinationType;
 
 	function onDeviceReady() {
-
-		navigator.contactsPhoneNumbers.list(function(contacts) {
-			console.log(contacts.length + ' contacts found');
-			for (var i = 0; i < contacts.length; i++) {
-				console.log(contacts[i].id + " - " + contacts[i].displayName);
-				var displayName=contacts[i].displayName;
-				for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
-					var phone = contacts[i].phoneNumbers[j];
-					console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
+		document.getElementById("btnPhonebook").addEventListener("click", function() {
+			navigator.contactsPhoneNumbers.list(function(contacts) {
+				// console.log(contacts.length + ' contacts found');
+				$("#displayPhonebookLength").html(contacts.length + ' contacts found');
+				for (var i = 0; i < contacts.length; i++) {
+					console.log(contacts[i].id + " - " + contacts[i].displayName);
+					var contactName = contacts[i].displayName;
+					for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+						var phone = contacts[i].phoneNumbers[j];
+						// var det = $("#displayPhonebook").html();
+						// console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
+						var details = contactName + " (" + phone.number + ")";
+						$("#displayPhonebook").append(details);
+					}
 				}
-				alert(displayName);
-			}
-			
-		}, function(error) {
-			console.error(error);
+			}, function(error) {
+				console.error(error);
+			});
 		});
-
 	};
-
-	function onPhotoDataSuccess(imageData) {
-
-	}
-
-	function onFail(message) {
-
-	}
-
-	function clearCache() {
-
-	}
 
 })();
 
